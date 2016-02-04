8927af3c9da873bfcb895f392036e3b8459bf6d7
jarnovnico <j.verhoogt@gmail.com>
2016-02-04 19:06:19 +0100

    onload functie?

--- vakken/webapp/opdracht5/static/js/app.js
+++ vakken/webapp/opdracht5/static/js/app.js
@@ -1,49 +1,51 @@
-(function() {
+window.onload = function(){
 
-	"use strict";
+	(function() {
 
-	var app = {
+		var app = {
 
-		// Methodes are defining functions to an object.
-		init: function() {
-			console.log("init app");
-			routes.init();
-		}
-	};
+			// Methodes are defining functions to an object.
+			init: function() {
+				console.log("init app");
+				routes.init();
+			}
+		};
 
-	var routes = {
-		
-		init: function() {
-			console.log("init routes");
+		var routes = {
 			
-			// The hashchange event fires when a window's hash changes.
-			window.addEventListener("hashchange", function(event) {
+			init: function() {
+				console.log("init routes");
+				
+				// The hashchange event fires when a window's hash changes.
+				window.addEventListener("hashchange", function(event) {
 
-				// The window.location read-only property returns a Location object with information about the current location of the document.
-				sections.toggle(window.location.hash);
-			});
+					// The window.location read-only property returns a Location object with information about the current location of the document.
+					sections.toggle(window.location.hash);
+				});
 
-			// sections.toggle(window.location.hash);
-		}
-	};
+				// sections.toggle(window.location.hash);
+			}
+		};
 
-	var sections = {
+		var sections = {
 
-		toggle: function() {
-			console.log("toggle sections");
+			toggle: function() {
+				console.log("toggle sections");
 
-			// var section = document.getElementById("section-navigatie");
+				// var section = document.getElementById("section-navigatie");
 
-			// section.onclick = function() {
-			// 	console.log("click");
-			// }
-		}
-	};
+				// section.onclick = function() {
+				// 	console.log("click");
+				// }
+			}
+		};
 
-	// Activate app.
-	app.init()
+		// Activate app.
+		app.init()
 
-})();
+	})();
+
+};
 
 // Bronnen:
 
