dfdae26533a9b6b2ed795be047fd49e6a307e75e
jarnovnico <j.verhoogt@gmail.com>
2016-02-07 17:58:58 +0100

    changes

--- vakken/webapp/opdracht5/static/js/app.js
+++ vakken/webapp/opdracht5/static/js/app.js
@@ -20,7 +20,7 @@ window.onload = function(){
 				window.addEventListener("hashchange", function(event) {
 
 					// The window.location read-only property returns a Location object with information about the current location of the document.
-					sections.toggle(window.location.hash);
+					sections.toggle();
 				});
 
 				// sections.toggle(window.location.hash);
@@ -32,7 +32,48 @@ window.onload = function(){
 			toggle: function() {
 				console.log("toggle sections");
 
-				// var section = document.getElementById("section-navigatie");
+				// var a = document.querySelectorAll(".section-content");
+
+				// if (a.style.display == "block") {
+				// 	a.style.display = "none";
+				// } else {
+				// 	a.style.display = "block";
+				// }
+
+
+
+				// var elems = document.getElementById(id);
+					
+				// for (var i = 0; i < elems.length; i += 1) {
+				// 	elems[i].style.display = "block";
+				// }
+
+
+
+				// var el = document.getElementById(id);
+
+				// if (el.style.display != "none") {
+				// 	el.style.display = "none";
+				// } else {
+				// 	el.style.display = "";
+				// }
+
+
+
+				var a = document.getElementById("section-lijstje"),
+					b = document.getElementById("section-startscherm");
+
+				if (a.style.display == "block") {
+					a.style.display = "none";
+					b.style.display = "block";
+				} else {
+					a.style.display = "block";
+					b.style.display = "none";
+				}
+
+
+
+				// var section = document.querySelectorAll(".section-content");
 
 				// section.onclick = function() {
 				// 	console.log("click");
